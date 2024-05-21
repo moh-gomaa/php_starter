51e5667642e5cd2f94f956d36b3415788db8dd2b
Mohamed Gomaa <mohamed.adel36@msa.edu.eg>
2024-05-21 04:15:04 +0300

    -Initialize php web app.
    -Working in routes and try the basic routing, route with required parameters, and finally with optional parameters.

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Basic route
+Route::get('/basicRoute', function () {
+    return 'Welcome in Basic Route';
+}) -> name('basicRoute');
+
+//Required parameter route
+Route::get('/routeWithID/{id}', function($id){
+    return 'Welcome in required parameter route. Parameter = '.$id;
+}) -> name('routeWithRequiredArgument');
+
+//Optional parameter route
+Route::get('/optionalParamRoute/{id?}', function($id = null){
+    return 'Welcome in optional parameter route. Parameter = '.$id;
+}) -> name('routeWithOptionalArgument');
